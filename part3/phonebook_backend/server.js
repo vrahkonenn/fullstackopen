6d2a49ac39b4943d5804c0059419ad3e95a29744
Veeti Rahkonen <vrahkonen02@gmail.com>
2026-05-23 13:46:05 +0300

    t3.8

---
+++ part3/phonebook_backend/server.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express() 
+var morgan = require('morgan')
+app.use(express.json())
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": "1"
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": "2"
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": "3"
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": "4"
+    } 
+]
+
+const generateId = () => {
+    return Math.floor(Math.random() * 10000)
+}
+// Routet
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    const amount = persons.length
+    const timestamp = new Date().toString()
+    response.end(`Phonebook has info for ${amount} people\n${timestamp}`)
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) return response.status(404).json({error: 'content missing'})
+        
+    if (persons.find(p => p.name === body.name)) return response.status(404).json({error: 'name must be unique'})
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    } 
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    
+})
