13bba016772494d188587872d358aa2c24bc64bd
Veeti Rahkonen <vrahkonen02@gmail.com>
2026-05-23 16:54:35 +0300

    dist

--- part3/phonebook_backend/server.js
+++ part3/phonebook_backend/server.js
@@ -4,6 +4,7 @@ var morgan = require('morgan')
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
